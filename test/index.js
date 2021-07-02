98d544a40eedde1fbefd7648e54c6feb28bb3bf8
Toby Miller <tobius@tobius.com>
2021-07-02 10:39:55 -0400

    Update codebase (last updated was three years ago)
    - Use new `.editorconfig`
    - Prefer Airbnb standards in `.eslintrc`
    - Ignore `*.swp` files in .gitignore (Vim-friendly)
    - Use MIT link instead of LICENSE file
    - Add `.nvmrc` and use Node v16
    - Update all devDependencies
    - Use chai.assert instead of chai.expect
    - Remove hard-coded eslint-disable rules
    - Add `PULL_REQUEST_TEMPLATE`
    - Fix lint errors

--- test/index.js
+++ test/index.js
@@ -1,84 +1,75 @@
-
 // import
-const
-	chai = require('chai'),
-	isTruthy = require('../index');
+const chai = require('chai');
+const isTruthy = require('../index');
 
-// IMPORTANT: only in tests
-process.on('unhandledRejection', () => {
-	/**/
-});
-
-// configure
-const expect = chai.expect;
+// enable stack traces
 chai.config.includeStack = true;
+
+// use chai assert
+const { assert } = chai;
 
 // test
 describe('isTruthy', () => {
 
-	it('should understand booleans', () => {
-		expect(isTruthy(true)).to.be.true;
-		expect(isTruthy('true')).to.be.true;
-		expect(isTruthy(false)).to.be.false;
-		expect(isTruthy('false')).to.be.false;
-	});
+  it('should understand booleans', () => {
+    assert.ok(isTruthy(true));
+    assert.ok(isTruthy('true'));
+    assert.notOk(isTruthy(false));
+    assert.notOk(isTruthy('false'));
+  });
 
-	it('should understand integers', () => {
-		expect(isTruthy(-100)).to.be.false;
-		expect(isTruthy('-100')).to.be.false;
-		expect(isTruthy(-1)).to.be.false;
-		expect(isTruthy('-1')).to.be.false;
-		expect(isTruthy(0)).to.be.false;
-		expect(isTruthy('0')).to.be.false;
-		expect(isTruthy(1)).to.be.true;
-		expect(isTruthy('1')).to.be.true;
-		expect(isTruthy(100)).to.be.false;
-		expect(isTruthy('100')).to.be.false;
-	});
+  it('should understand integers', () => {
+    assert.notOk(isTruthy(-100));
+    assert.notOk(isTruthy('-100'));
+    assert.notOk(isTruthy(-1));
+    assert.notOk(isTruthy('-1'));
+    assert.notOk(isTruthy(0));
+    assert.notOk(isTruthy('0'));
+    assert.ok(isTruthy(1));
+    assert.ok(isTruthy('1'));
+    assert.notOk(isTruthy(100));
+    assert.notOk(isTruthy('100'));
+  });
 
-	it('should understand floats', () => {
-		expect(isTruthy(-100.5)).to.be.false;
-		expect(isTruthy('-100.5')).to.be.false;
-		expect(isTruthy(-1.5)).to.be.false;
-		expect(isTruthy('-1.5')).to.be.false;
-		expect(isTruthy(0.0)).to.be.false;
-		expect(isTruthy('0.0')).to.be.false;
-		expect(isTruthy(1.0)).to.be.true;
-		expect(isTruthy('1.0')).to.be.true;
-		expect(isTruthy(1.5)).to.be.false;
-		expect(isTruthy('1.5')).to.be.false;
-		expect(isTruthy(100.5)).to.be.false;
-		expect(isTruthy('100.5')).to.be.false;
-	});
+  it('should understand floats', () => {
+    assert.notOk(isTruthy(-100.5));
+    assert.notOk(isTruthy('-100.5'));
+    assert.notOk(isTruthy(-1.5));
+    assert.notOk(isTruthy('-1.5'));
+    assert.notOk(isTruthy(0.0));
+    assert.notOk(isTruthy('0.0'));
+    assert.ok(isTruthy(1.0));
+    assert.ok(isTruthy('1.0'));
+    assert.notOk(isTruthy(1.5));
+    assert.notOk(isTruthy('1.5'));
+    assert.notOk(isTruthy(100.5));
+    assert.notOk(isTruthy('100.5'));
+  });
 
-	it('should understand null', () => {
-		expect(isTruthy(null)).to.be.false;
-		expect(isTruthy('null')).to.be.false;
-	});
+  it('should understand null', () => {
+    assert.notOk(isTruthy(null));
+    assert.notOk(isTruthy('null'));
+  });
 
-	it('should understand undefined', () => {
-		/* eslint-disable no-undefined */
-		expect(isTruthy(undefined)).to.be.false;
-		/* eslint-enable no-undefined */
-		expect(isTruthy('undefined')).to.be.false;
-	});
+  it('should understand undefined', () => {
+    assert.notOk(isTruthy(undefined));
+    assert.notOk(isTruthy('undefined'));
+  });
 
-	it('should understand NaN', () => {
-		expect(isTruthy(NaN)).to.be.false;
-		expect(isTruthy('NaN')).to.be.false;
-	});
+  it('should understand NaN', () => {
+    assert.notOk(isTruthy(NaN));
+    assert.notOk(isTruthy('NaN'));
+  });
 
-	it('should understand Infinity', () => {
-		expect(isTruthy(Infinity)).to.be.false;
-		expect(isTruthy('Infinity')).to.be.false;
-	});
+  it('should understand Infinity', () => {
+    assert.notOk(isTruthy(Infinity));
+    assert.notOk(isTruthy('Infinity'));
+  });
 
-	it('should understand "human truthy" strings', () => {
-		expect(isTruthy('on')).to.be.true;
-		expect(isTruthy('active')).to.be.true;
-		expect(isTruthy('off')).to.be.false;
-		expect(isTruthy('inactive')).to.be.false;
-	});
-
+  it('should understand "human truthy" strings', () => {
+    assert.ok(isTruthy('on'));
+    assert.ok(isTruthy('active'));
+    assert.notOk(isTruthy('off'));
+    assert.notOk(isTruthy('inactive'));
+  });
 });
-
